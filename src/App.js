75e3b6ddfceb488cdd3e6f01a257d2d632187fbf
anna-ip <anna.almestal@gmail.com>
2020-04-28 14:24:31 +0200

    Set up the structure and installed dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { Message } from './components/Message'
+
+const reducer = combineReducers({
+  message: message.reducer,
+})
+
+const store = configureStore({ reducer })
 
 export const App = () => {
   return (
     <div>
+      <Provider store={store}>
+        <Message />
       Find me in src/app.js!
+      </Provider>
     </div>
   )
 }
---
+++ src/reducer/messages.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const message = createSlice({
+
+	name: 'message',
+	initialState: {
+		text: []
+	},
+
+	reducers: {
+
+	}
+
+})
